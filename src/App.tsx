93fddbae77a6e1061d3dfe276dc81af3faf1483c
William Mibelli <william.mibelli@gmail.com>
2024-11-12 08:41:43 +0100

    usestate for pokemonIndex

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-
-import './App.css'
-import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
@@ -9,17 +9,48 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
-
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState<number>(0);
+
+  const onIncrementCLick = () => {
+    setPokemonIndex((previous) => previous + 1);
+  };
+
+  const onDecrementCLick = () => {
+    setPokemonIndex((previous) => previous - 1);
+  };
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <div>
+        {pokemonIndex > 0 && (
+          <button onClick={onDecrementCLick}>Précédent</button>
+        )}
+        {pokemonIndex < pokemonList.length - 1 && (
+          <button onClick={onIncrementCLick}>Suivant</button>
+        )}
+      </div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
