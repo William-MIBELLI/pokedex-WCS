898bd0961995cbf0e6c48f06a01ba3188af5e116
William Mibelli <william.mibelli@gmail.com>
2024-11-12 08:17:29 +0100

    dynamic data displayed on pokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,25 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const PokemonCard = () => {
+  const pokemon = pokemonList[0];
+  const { name, imgSrc } = pokemon;
   return (
     <div>
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur img" />
+        {imgSrc ? <img src={imgSrc} alt={`${name}'s img`} /> : <p>???</p>}
       </figure>
-      <figcaption>
-        Bulbasaur
-      </figcaption>
+      <figcaption>{name}</figcaption>
     </div>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
